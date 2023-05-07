9004a454463d51c92c2854b9fb6d89d4543ec97f
YM <ymironchyk@gmail.com>
2023-05-06 18:25:23 -0700

    Fixed stepper motor, working now

---
+++ CPE 301L/Final Project/Stepper/Stepper.ino
@@ -0,0 +1,99 @@
+// Stepper motor library
+#include <Stepper.h>
+
+// Define # of steps / revolution
+const int stepsPerRev = 2048;
+
+// Creates stepper class instance
+// Pin order in function parameters is: IN1 / IN2 / IN3 / IN4
+Stepper myStepper = Stepper(stepsPerRev, 29, 27, 25, 23);
+
+int ventPosition = 0;
+
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+ 
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
+
+void setup() {
+  // Set the speed to 5 rpm:
+  myStepper.setSpeed(5);
+  
+  // Begin Serial communication at a baud rate of 9600:
+  Serial.begin(9600);
+
+  // setup the ADC
+  adc_init();
+}
+
+void loop() {
+  int stepPotent = adc_read(0);
+  Serial.println(stepPotent);
+  if(stepPotent > 420 && ventPosition < 100){
+    myStepper.step(205);
+    ventPosition += 10;
+  }
+  else if(stepPotent < 210 && ventPosition > 0){
+    myStepper.step(-205);
+    ventPosition -= 10;
+  }
+  
+  delay(5);
+  
+//  // Step one revolution in one direction:
+//  Serial.println("clockwise");
+//  myStepper.step(2048);
+//  delay(200);
+//  
+//  // Step one revolution in the other direction:
+//  Serial.println("counterclockwise");
+//  myStepper.step(-2048);
+//  delay(200);
+}
+
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11110111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00001000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+}
